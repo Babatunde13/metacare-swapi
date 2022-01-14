e64f7d17876eb1664070bdd347dcab958ab17d00
Babatunde Koiki <koikibabatunde14@gmail.com>
2022-01-14 19:36:24 +0100

    (prod) release v1.0.0

--- src/controllers/get_movies.ctrl.ts
+++ src/controllers/get_movies.ctrl.ts
@@ -1,27 +1,36 @@
 import { Req, Res } from '../api_contracts/get_movies.ctrl.contract'
 import { getStarWars } from '../utils/request.utils'
+// import getCommentRepository from '../entities/comments.entity'
 
 export default async function getMoviesCtrl(req: Req): Res {
+    console.log(req.ip)
+    console.log(req.headers['x-forwarded-for'] || req.connection.remoteAddress)
     const movies = await getStarWars()
     if (!movies.success) return movies
-    // sort by createdAt field
+    const moviesResult = []
+    for (const movie of movies.data.movies.results) {
+        // const commentModel = await getCommentRepository().findAndCount({
+        //     where: {
+        //         movieId: +movie.id
+        //     }
+        // })
+        const commentModel = [{
+            id: movie.id,
+            movieId: movie.id,
+            comment: 'Something about this movie',
+        }, 1]
+        moviesResult.push({
+            id: movie.id,
+            title: movie.title.toUpperCase(),
+            opening_crawl: movie.opening_crawl,
+            commentCount: commentModel[1] as number,
+            createdAt: new Date(movie.created),
+        })
+    }
     return {
         ...movies,
         data: {
-            movies: movies.data.movies.results.map((movie: any) => {
-                const commentModel = [{
-                    id: movie.id,
-                    movieId: movie.id,
-                    comment: 'Something about this movie',
-                }]
-                return {
-                    id: movie.id,
-                    title: movie.title.toUpperCase(),
-                    opening_crawl: movie.opening_crawl,
-                    commentCount: commentModel.length,
-                    createdAt: new Date(movie.created),
-                }
-            }).sort((a: any, b: any) => {
+            movies: moviesResult.sort((a: any, b: any) => {
                 return b.createdAt - a.createdAt
             }),
             metaData: {
